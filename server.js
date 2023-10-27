927adfdecbe9b054ccb4852e59dbbdf4af38bb40
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-10-27 17:17:35 -0400

    imported dependencies and created connections and prompts for server.js

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 const inquirer = require('inquirer');
+const express = require('express');
+const mysql = require('mysql2');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+// Connecting the database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      database: 'employee_db',
+      password: 'Mont@na456'
+    },
+    console.log(`Connected to employee database,`)
+);
 
 // THEN I am presented with the following options: 
 // view all departments, 
@@ -7,3 +23,20 @@ const inquirer = require('inquirer');
 // add a role, 
 // add an employee, 
 // and update an employee role
+function menuPrompt() {
+    inquirer.prompt ({
+        type: 'list',
+        name: 'start',
+        message: 'What would you like to do?',
+        choices: [
+            'View all department',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update and employee role',
+            'Exit'
+        ],
+    })
+}
